af28f8ddb6b5204318c5dcb059749d98806eea8d
Dan Tripp <daniel.john.tripp@gmail.com>
2025-03-22 19:39:55 -0400

    .

--- from-scratchpad/example2.py
+++ from-scratchpad/example2.py
@@ -7,6 +7,23 @@ import versionInfo
 from logHandler import log
 import speech
 import tones 
+import gettext
+from synthDriverHandler import getSynth
+
+_ = gettext.gettext
+
+# speech/SSML processing borrowed from NVDA's mathPres/mathPlayer.py
+from speech.commands import (
+    BeepCommand,
+    PitchCommand,
+    VolumeCommand,
+    RateCommand,
+    LangChangeCommand,
+    BreakCommand,
+    CharacterModeCommand,
+    PhonemeCommand,
+    IndexCommand,
+)
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
@@ -16,12 +33,20 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def event_gainFocus(self, obj, nextHandler):
 		try:
-			#log.info(f'data-ssml: {obj.name} {obj.description}')
-			#log.info(f'data-ssml: {dir(obj)}')
+			callNextHandler = True
 			if obj.description:
-				log.info(f'data-ssml: got description: {obj.description} (for name = "{obj.name}")')
+				if 1:
+					log.info(f'data-ssml: got description: {obj.description} (for name = "{obj.name}")')
+					#synth = getSynth()
+					#log.info(f'data-ssml: supported commands: {str(synth.supportedCommands)}')
+				#speech.speakMessage(obj.description)
+				ipa = obj.description
+				#speech.speak([PhonemeCommand(ipa, obj.name)])
+				speech.speak([CharacterModeCommand(True), obj.name, CharacterModeCommand(False)])
+				callNextHandler = False
 			tones.beep(200, 20)
-			nextHandler()
+			if callNextHandler:
+				nextHandler()
 		except Exception as e:
 			log.exception(e)
 			# Translators: this message directs users to look in the log file
