0967c43d5f89751a129b411d149e0e14ac8d553b
Dan Tripp <daniel.john.tripp@gmail.com>
2025-03-22 14:11:23 -0400

    .

--- from-scratchpad/example2.py
+++ from-scratchpad/example2.py
@@ -4,17 +4,25 @@ import globalPluginHandler
 from scriptHandler import script
 import ui
 import versionInfo
-
+from logHandler import log
+import speech
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
     #@script(gesture="kb:NVDA+shift+v")
     #def script_announceNVDAVersion(self, gesture):
     #    ui.message(versionInfo.version)
 
     def event_gainFocus(self, obj, nextHandler):
-        import tones
-        tones.beep(300, 100)
-        nextHandler()
+        try:
+            import tones
+            tones.beep(500, 50)
+            nextHandler()
+        except Exception as e:
+            log.exception(e)
+            # Translators: this message directs users to look in the log file
+            speech.speakMessage("Error in data-ssml plugin.  See NVDA error log for details.")
+        
 
 
 
