06935543a935a7c34fe55acef7a47844561e12fd
Dan Tripp <daniel.john.tripp@gmail.com>
2025-03-23 01:07:11 -0400

    .

--- nvda-add-on-data-ssml.js
+++ nvda-add-on-data-ssml.js
@@ -1,14 +1,34 @@
 
-function init() {
-	turnDataSsmlIntoAriaDescriptions();
+function go() {
+	turnDataSsmlIntoZeroWidthChars();
 }
 
-function turnDataSsmlIntoAriaDescriptions() {
+function encodeDataSsmlAsZeroWidthCharsStr(str_) {
+    let encoder = new TextEncoder();
+    let utf8Bytes = encoder.encode(str_);
+    let result = '';
+
+    for (let byte of utf8Bytes) {
+        for (let i = 7; i >= 0; i--) {
+            let bit = (byte >> i) & 1;
+            result += bit === 0 ? '\u200C' : '\u200D';
+        }
+    }
+
+    return result;
+}
+
+function turnDataSsmlIntoZeroWidthChars() {
 	for(let elem of document.querySelectorAll('[data-ssml]')) {
 		let dataSsmlValue = elem.getAttribute('data-ssml');
 		if(!dataSsmlValue) continue;
-		
+		let zeroWidthCharsStr = encodeDataSsmlAsZeroWidthCharsStr(dataSsmlValue);
+		const START_MARKER = '\u2060\u2062\u2063', END_MARKER = '\u2063\u2062\u2060';
+		elem.insertBefore(document.createTextNode(START_MARKER+zeroWidthCharsStr+END_MARKER), elem.firstChild);
+		elem.appendChild(document.createTextNode(START_MARKER+END_MARKER));
 	}
 }
 
-init();
+window.addEventListener("load", function(event) {
+	go();
+});
