1799d9959bfb1c2f2df521a9e91bf3a9550fe392
Dan Tripp <daniel.john.tripp@gmail.com>
2025-03-23 20:33:15 -0400

    .

--- from-scratchpad/data-ssml.py
+++ from-scratchpad/data-ssml.py
@@ -30,39 +30,6 @@ def logInfo(str_):
 	log.info(f'data-ssml: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {str_}')
 
 
-
-
-# Matches the javascript encoding side.  If you change that then change this, and vice versa. 
-START_MARKER = '\u2060\u2062\u2063'
-END_MARKER = '\u2063\u2062\u2060'
-ZERO_CHAR = '\u200C'
-ONE_CHAR = '\u200D'
-
-def decodeSsmlZeroWidthCharsBinary(str_):
-	results = []
-	start_index = 0
-
-	while True:
-		start_pos = str_.find(START_MARKER, start_index)
-		if start_pos == -1:
-			break
-		start_pos += len(START_MARKER)
-		end_pos = str_.find(END_MARKER, start_pos)
-		if end_pos == -1:
-			break
-
-		encoded = str_[start_pos:end_pos]
-		if encoded:
-			bits = ''.join('0' if c == ZERO_CHAR else '1' for c in encoded)
-			byte_values = [int(bits[i:i+8], 2) for i in range(0, len(bits), 8)]
-			decoded_str = bytes(byte_values).decode('utf-8')
-			results.append(decoded_str)
-
-		start_index = end_pos + len(END_MARKER)
-
-	return results
-
-
 def decodeSingleStrOctal(str_):
 	zero_width_char_map = {
 		'\uFFF9': '0',
@@ -102,20 +69,14 @@ def turnSsmlIntoSpeechCommandList(ssmlAsJsonStr_, nonSsmlStr_):
 
 # returns a list where each element is a string or a speech command.  
 def decodeAllStrsOctal(str_):
+	# Matches the javascript encoding side.  If you change that then change this, and vice versa. 
 	START_MARKER_OCTAL = '\u2062\u2063'
 	END_MARKER_OCTAL = '\u2063\u2062'
 
-	if 0: # tdr 
-		if (START_MARKER_OCTAL in str_) != (END_MARKER_OCTAL in str_):
-			logInfo('markers bad')
-		elif (START_MARKER_OCTAL in str_) and (END_MARKER_OCTAL in str_):
-			pass
-			#logInfo('markers good')
-		else:
-			pass
-
-	if (START_MARKER_OCTAL in str_) != (END_MARKER_OCTAL in str_):
-		raise Exception(f'markers bad.  str was: {str_}')
+	startMarkerCount = str_.count(START_MARKER_OCTAL)
+	endMarkerCount = str_.count(START_MARKER_OCTAL)
+	if not (startMarkerCount == endMarkerCount and startMarkerCount in (0, 2)):
+		raise Exception(f'markers are bad.  str was: "{str_}"')
 
 	r = []
 	searchStartPos = 0
@@ -139,36 +100,17 @@ def decodeAllStrsOctal(str_):
 			except Exception as e:
 				log.exception(e)
 				logInfo(f'encoded string was: {encodedSsml}')
+		else:
+			logInfo(f'empty encoded string i.e. macro end marker.')
 
 		searchStartPos = endMarkerStartPos + len(END_MARKER_OCTAL)
 
 	return r
 
 
-# Matches the javascript encoding side.  If you change that then change this, and vice versa. 
-START_MARKER_RE = '\[ssml-start\]'
-END_MARKER_RE = '\[ssml-end\]'
-
-def decodeSsmlEncodedAsBase64(str_):
-    decoded_payloads = []
-    pattern = re.compile(f'{START_MARKER_RE}(.*?){END_MARKER_RE}')
-    
-    for match in pattern.finditer(str_):
-        base64_str = match.group(1)
-        try:
-            decoded_bytes = base64.b64decode(base64_str)
-            decoded_text = decoded_bytes.decode('utf-8')
-            decoded_payloads.append(decoded_text)
-        except Exception as e:
-            decoded_payloads.append(f"[decode error: {e}]")
-
-    return decoded_payloads
-
-
 original_synth_speak = synthDriverHandler.getSynth().speak
 
 def custom_synth_speak(speechSequence, *args, **kwargs):
-	#logInfo(f'here 2. {str(speechSequence)}')
 	modified_sequence = []
 	for element in speechSequence:
 		if isinstance(element, str):
@@ -178,45 +120,11 @@ def custom_synth_speak(speechSequence, *args, **kwargs):
 	return original_synth_speak(modified_sequence, *args, **kwargs)
 
 
-
-
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		synthDriverHandler.getSynth().speak = custom_synth_speak
-
-	#@script(gesture="kb:NVDA+shift+v")
-	#def script_announceNVDAVersion(self, gesture):
-	#    ui.message(versionInfo.version)
-
-	def event_gainFocus(self, obj, nextHandler):
-		if 1:
-			nextHandler()
-			return
-		try:
-			callNextHandler = True
-			if 0:
-				#ui.message(f'location: {obj.location}')
-				#ui.message(f'name: {obj.name}')
-				logInfo(obj.name)
-			if obj.description:
-				if 0:
-					logInfo(f'got description: {obj.description} (for name = "{obj.name}")')
-					#log.info(f'data-ssml: supported commands: {str(synth.supportedCommands)}')
-				#speech.speakMessage(obj.description)
-				ipa = obj.description
-				#speech.speak([PhonemeCommand(ipa, obj.name)])
-				speech.speak([CharacterModeCommand(True), obj.name, CharacterModeCommand(False)])
-				callNextHandler = False
-			tones.beep(200, 20)
-			if callNextHandler:
-				nextHandler()
-		except Exception as e:
-			log.exception(e)
-			# Translators: this message directs users to look in the log file
-			speech.speakMessage("Error in data-ssml plugin.  See NVDA error log for details.")
-
 
 
 
--- nvda-add-on-data-ssml.js
+++ nvda-add-on-data-ssml.js
@@ -94,18 +94,10 @@ function encodeAllDataSsmlAttribs() {
 	for(let elem of document.querySelectorAll('[data-ssml]')) {
 		let dataSsmlValue = elem.getAttribute('data-ssml');
 		if(!dataSsmlValue) continue;
-		const USE_BASE64 = false;
-		if(USE_BASE64) {
-			let dataSsmlValueEncoded = encodeDataSsmlAsBase64(dataSsmlValue);
-			const START_MARKER = '[ssml-start]', END_MARKER = '[ssml-end]';
-			elem.insertBefore(document.createTextNode(START_MARKER+dataSsmlValueEncoded+END_MARKER), elem.firstChild);
-			elem.appendChild(document.createTextNode(START_MARKER+END_MARKER))
-		} else {
-			let zeroWidthCharsStr = encodeSsmlAsZeroWidthCharsOctal(dataSsmlValue);
-			const START_MARKER = '\u2062\u2063', END_MARKER = '\u2063\u2062';
-			elem.insertBefore(document.createTextNode(START_MARKER+zeroWidthCharsStr+END_MARKER), elem.firstChild);
-			elem.appendChild(document.createTextNode(START_MARKER+END_MARKER));
-		}
+		let encodedSsml = encodeSsmlAsZeroWidthCharsOctal(dataSsmlValue);
+		const START_MARKER = '\u2062\u2063', END_MARKER = '\u2063\u2062';
+		elem.insertBefore(document.createTextNode(START_MARKER+encodedSsml+END_MARKER), elem.firstChild);
+		elem.appendChild(document.createTextNode(START_MARKER+END_MARKER));
 	}
 }
 
